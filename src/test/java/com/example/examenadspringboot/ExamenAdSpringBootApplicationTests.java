ecf177b31f91ae7fb8c8cc651924f4e03a0b15eb
jorgeoj <jorge2002oj@gmail.com>
2024-03-04 11:38:22 +0100

    Initial commit

---
+++ src/test/java/com/example/examenadspringboot/ExamenAdSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenadspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenAdSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
